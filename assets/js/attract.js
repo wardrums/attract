bf5024ee07aa7381754cd154caa6dd5d54671f37
fsiddi <francesco.siddi@gmail.com>
2012-07-09 12:50:35 +0200

    Removed pagination code
    It might be added back in the future

--- assets/js/attract.js
+++ assets/js/attract.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+	// Enabling plugins
+
 	$('#statsModal').modal({
 		show: false
 	});
@@ -21,6 +23,9 @@ $(document).ready(function() {
 	
 	$(".chzn-select").chosen({no_results_text: "Select owner..."}); 
 	
+	
+	
+	// Functions
 	
 	function queryShots(status, owner) {
 		// Simple log print
@@ -55,79 +60,6 @@ $(document).ready(function() {
 	});
 
     
-    
-    /*
-    var maxRows = 100;
-	$('.paginated').each(function() {
-	    var cTable = $(this);
-	    var cRows = cTable.find('tr:gt(0)');
-	    var cRowCount = cRows.size();
-	    
-	    if (cRowCount < maxRows) {
-	        return;
-	    }
-	
-	    cRows.each(function(i) {
-	        $(this).find('td:first').text(function(j, val) {
-	           //return (i + 1) + " - " + val;
-	           return val;
-	        });
-	    });
-	
-	    cRows.filter(':gt(' + (maxRows - 1) + ')').hide();
-	
-	
-	    var cPrev = cTable.siblings('.prev');
-	    var cNext = cTable.siblings('.next');
-	
-	    cPrev.addClass('disabled');
-	
-	    cPrev.click(function() {
-	        var cFirstVisible = cRows.index(cRows.filter(':visible'));
-	        
-	        if (cPrev.hasClass('disabled')) {
-	            return false;
-	        }
-	        
-	        cRows.hide();
-	        if (cFirstVisible - maxRows - 1 > 0) {
-	            cRows.filter(':lt(' + cFirstVisible + '):gt(' + (cFirstVisible - maxRows - 1) + ')').show();
-	        } else {
-	            cRows.filter(':lt(' + cFirstVisible + ')').show();
-	        }
-	
-	        if (cFirstVisible - maxRows <= 0) {
-	            cPrev.addClass('disabled');
-	        }
-	        
-	        cNext.removeClass('disabled');
-	
-	        return false;
-	    });
-	
-	    cNext.click(function() {
-	        var cFirstVisible = cRows.index(cRows.filter(':visible'));
-	        
-	        if (cNext.hasClass('disabled')) {
-	            return false;
-	        }
-	        
-	        cRows.hide();
-	        cRows.filter(':lt(' + (cFirstVisible +2 * maxRows) + '):gt(' + (cFirstVisible + maxRows - 1) + ')').show();
-	
-	        if (cFirstVisible + 2 * maxRows >= cRows.size()) {
-	            cNext.addClass('disabled');
-	        }
-	        
-	        cPrev.removeClass('disabled');
-	
-	        return false;
-	    });
-	
-	});	
-	*/
-		
-	
 	function selectThis(row_id, cell) {
 		// Simple log print
 		console.log("We are selecting the " + cell + " in row " + row_id);
