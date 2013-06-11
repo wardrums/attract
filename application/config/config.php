4efa78fb679179bd7e1c9bb5c5c9dbc09832ad01
fsiddi <francesco.siddi@gmail.com>
2013-06-11 18:29:28 -0400

    Authentication library for user management

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'w0VaZYqBhz8XeAL5X10WYfrQCI6lq6zT';
 
 /*
 |--------------------------------------------------------------------------
