82bb6820b193ceef655f66b12ff9601b6b0cfa46
fsiddi <francesco.siddi@gmail.com>
2013-10-09 01:22:50 +0200

    Error cleanup for modal sequences editor

--- application/views/sequences/index.php
+++ application/views/sequences/index.php
@@ -29,7 +29,7 @@ if ($this->session->flashdata('message') != '')
 		
 	<?php foreach ($sequences as $sequence): ?>
     	<tr class="sequence_<?php echo $sequence['sequence_id'] ?>">
-    		<td><a href="/sequences/edit/<?php echo $sequence['sequence_id'] ?>" data-toggle="modal"><?php echo $sequence['sequence_name'] ?></a></td>
+    		<td><a href="#" sequence="/sequences/edit/<?php echo $sequence['sequence_id'] ?>" data-toggle="modal"><?php echo $sequence['sequence_name'] ?></a></td>
     		<td><?php echo $sequence['sequence_description'] ?></td>
     	</tr>
 	<?php endforeach ?>
@@ -52,7 +52,7 @@ $(document).ready(function() {
 	// Focuses on first input textbox after it loads the window.
 	$('[data-toggle="modal"]').click(function(e) {
 		e.preventDefault();
-		var url = $(this).attr('href');
+		var url = $(this).attr('sequence');
 		if (url.indexOf('#') == 0) {
 			$(url).modal('open');
 		} else {
