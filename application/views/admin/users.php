284eb21cad850e2efc466ee994ebdc186201c2e4
fsiddi <francesco.siddi@gmail.com>
2013-11-14 01:47:27 +0100

    Admin users layout tweak

--- application/views/admin/users.php
+++ application/views/admin/users.php
@@ -4,31 +4,39 @@
 
 <h2><?php echo $title ?></h2>
 
-
-<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" id="users">
+<table cellpadding="0" cellspacing="0" border="0" class="table table-striped" id="users">
 	<thead>
 		<tr>
 			<th>Name</th>
-			<th>Surname</th>
 			<th>Email</th>
 			<th>Group</th>
-			<th>Status</th>
 			<th>Edit</th>
 		</tr>
 	</thead>
 	<tbody>
 	<?php foreach ($users as $user):?>
+		
+		
 		<tr>
-			<td><?php echo $user->first_name;?></td>
-			<td><?php echo $user->last_name;?></td>
+			<td><?php echo $user->first_name;?> <?php echo $user->last_name;?></td>
 			<td><?php echo $user->email;?></td>
 			<td>
 				<?php foreach ($user->groups as $group):?>
-					<?php echo anchor("auth/edit_group/".$group->id, $group->name) ;?><br />
+					<span class="label"><?php echo $group->name;?></span>
                 <?php endforeach?>
 			</td>
-			<td><?php echo ($user->active) ? anchor("auth/deactivate/".$user->id, lang('index_active_link')) : anchor("auth/activate/". $user->id, lang('index_inactive_link'));?></td>
-			<td><?php echo anchor("auth/edit_user/".$user->id, 'Edit') ;?></td>
+			<td>
+				<div class="btn-group">
+				  <?php echo anchor("auth/edit_user/".$user->id, 'Edit', 'class="btn btn-mini"') ;?>
+				  <button class="btn btn-mini dropdown-toggle" data-toggle="dropdown">
+				    <span class="caret"></span>
+				  </button>
+				  <ul class="dropdown-menu pull-right">
+				    <li><?php echo ($user->active) ? anchor("auth/deactivate/".$user->id, lang('index_active_link')) : anchor("auth/activate/". $user->id, lang('index_inactive_link'));?></li>
+				    <li><?php echo(anchor('/admin/_post_delete_user/'.$user->id , 'Delete')) ?></li>
+				  </ul>
+				</div>
+			</td>
 		</tr>
 	<?php endforeach;?>
 		
@@ -36,10 +44,8 @@
 	<tfoot>
 		<tr>
 			<th>Name</th>
-			<th>Surname</th>
 			<th>Email</th>
 			<th>Group</th>
-			<th>Status</th>
 			<th>Edit</th>
 		</tr>
 	</tfoot>
