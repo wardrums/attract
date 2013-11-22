74add25cd17f1f6b8af518ac8ef1a9c0f95ba7a5
fsiddi <francesco.siddi@gmail.com>
2013-11-22 01:31:08 +0100

    Attract installer 1.0

---
+++ install/assets/install.sql
@@ -0,0 +1,459 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: localhost (MySQL 5.5.29)
+# Database: attract-ci
+# Generation Time: 2013-11-22 00:26:07 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table attachments
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `attachments`;
+
+CREATE TABLE `attachments` (
+  `attachment_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `attachment_name` varchar(256) NOT NULL,
+  `attachment_path` varchar(512) NOT NULL,
+  `attachment_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`attachment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table comments
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `comments`;
+
+CREATE TABLE `comments` (
+  `comment_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `shot_id` int(11) unsigned NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `comment_body` text,
+  `comment_edit_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `comment_creation_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table comments_attachments
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `comments_attachments`;
+
+CREATE TABLE `comments_attachments` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `attachment_id` int(11) unsigned NOT NULL,
+  `comment_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table files
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `files`;
+
+CREATE TABLE `files` (
+  `file_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `file_path` varchar(255) DEFAULT NULL,
+  `file_settings` mediumtext,
+  PRIMARY KEY (`file_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table groups
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `groups`;
+
+CREATE TABLE `groups` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `description` varchar(100) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `groups` WRITE;
+/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
+
+INSERT INTO `groups` (`id`, `name`, `description`)
+VALUES
+	(1,'admin','Administrator'),
+	(2,'members','General User'),
+	(3,'artists','People who work on art');
+
+/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table login_attempts
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `login_attempts`;
+
+CREATE TABLE `login_attempts` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `ip_address` varbinary(16) NOT NULL,
+  `login` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `time` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table migrations
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `migrations`;
+
+CREATE TABLE `migrations` (
+  `version` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+
+INSERT INTO `migrations` (`version`)
+VALUES
+	(3);
+
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table scenes
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `scenes`;
+
+CREATE TABLE `scenes` (
+  `scene_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `sequence_id` int(11) NOT NULL,
+  `scene_name` text NOT NULL,
+  `scene_description` text,
+  PRIMARY KEY (`scene_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `scenes` WRITE;
+/*!40000 ALTER TABLE `scenes` DISABLE KEYS */;
+
+INSERT INTO `scenes` (`scene_id`, `sequence_id`, `scene_name`, `scene_description`)
+VALUES
+	(1,1,'Default Scene',NULL);
+
+/*!40000 ALTER TABLE `scenes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table sequences
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `sequences`;
+
+CREATE TABLE `sequences` (
+  `sequence_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `show_id` int(11) DEFAULT NULL,
+  `sequence_name` text NOT NULL,
+  `sequence_description` text,
+  PRIMARY KEY (`sequence_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `sequences` WRITE;
+/*!40000 ALTER TABLE `sequences` DISABLE KEYS */;
+
+INSERT INTO `sequences` (`sequence_id`, `show_id`, `sequence_name`, `sequence_description`)
+VALUES
+	(1,1,'Default Sequence','Sequence Description');
+
+/*!40000 ALTER TABLE `sequences` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table settings
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `settings`;
+
+CREATE TABLE `settings` (
+  `setting_id` int(11) NOT NULL AUTO_INCREMENT,
+  `setting_group` int(11) DEFAULT NULL,
+  `setting_name` varchar(250) DEFAULT NULL,
+  `setting_value` text,
+  `setting_help` text,
+  `setting_options` text,
+  `setting_hidden` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`setting_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `settings` WRITE;
+/*!40000 ALTER TABLE `settings` DISABLE KEYS */;
+
+INSERT INTO `settings` (`setting_id`, `setting_group`, `setting_name`, `setting_value`, `setting_help`, `setting_options`, `setting_hidden`)
+VALUES
+	(1,0,'current_show','1','You can pick only one show at a time.',NULL,NULL);
+
+/*!40000 ALTER TABLE `settings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table shot_tasks_users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `shot_tasks_users`;
+
+CREATE TABLE `shot_tasks_users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `shot_task_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table shots
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `shots`;
+
+CREATE TABLE `shots` (
+  `shot_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `scene_id` int(11) DEFAULT NULL,
+  `shot_name` varchar(256) DEFAULT NULL,
+  `shot_description` text,
+  `shot_duration` int(11) DEFAULT NULL,
+  `status_id` int(11) DEFAULT NULL,
+  `shot_notes` text,
+  `shot_order` int(11) DEFAULT NULL,
+  `shot_creation_date` datetime DEFAULT NULL,
+  `shot_edit_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`shot_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table shots_files
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `shots_files`;
+
+CREATE TABLE `shots_files` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `shot_id` int(11) NOT NULL,
+  `file_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `shots_files` WRITE;
+/*!40000 ALTER TABLE `shots_files` DISABLE KEYS */;
+
+INSERT INTO `shots_files` (`id`, `shot_id`, `file_id`)
+VALUES
+	(1,1,1);
+
+/*!40000 ALTER TABLE `shots_files` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table shots_tasks
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `shots_tasks`;
+
+CREATE TABLE `shots_tasks` (
+  `shot_task_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `shot_id` int(11) DEFAULT NULL,
+  `task_id` int(11) DEFAULT NULL,
+  `status_id` int(11) DEFAULT NULL,
+  `shot_task_order` int(11) DEFAULT NULL,
+  PRIMARY KEY (`shot_task_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table shots_users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `shots_users`;
+
+CREATE TABLE `shots_users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `shot_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='This table is for shot supervisors';
+
+
+
+# Dump of table shows
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `shows`;
+
+CREATE TABLE `shows` (
+  `show_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `show_name` varchar(256) NOT NULL DEFAULT '',
+  `show_description` text,
+  `show_path` text,
+  PRIMARY KEY (`show_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `shows` WRITE;
+/*!40000 ALTER TABLE `shows` DISABLE KEYS */;
+
+INSERT INTO `shows` (`show_id`, `show_name`, `show_description`, `show_path`)
+VALUES
+	(1,'Default Show','Default description','/show/path/');
+
+/*!40000 ALTER TABLE `shows` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table statuses
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `statuses`;
+
+CREATE TABLE `statuses` (
+  `status_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `status_name` varchar(256) NOT NULL DEFAULT '',
+  `status_color` tinytext CHARACTER SET latin1,
+  PRIMARY KEY (`status_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `statuses` WRITE;
+/*!40000 ALTER TABLE `statuses` DISABLE KEYS */;
+
+INSERT INTO `statuses` (`status_id`, `status_name`, `status_color`)
+VALUES
+	(1,'in_progress',NULL),
+	(2,'todo',NULL),
+	(3,'final_1',NULL),
+	(4,'fix',NULL),
+	(5,'review',NULL),
+	(6,'CBB',NULL);
+
+/*!40000 ALTER TABLE `statuses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table tasks
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `tasks`;
+
+CREATE TABLE `tasks` (
+  `task_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `task_name` varchar(128) NOT NULL DEFAULT '',
+  `task_order` int(11) DEFAULT NULL,
+  PRIMARY KEY (`task_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+
+INSERT INTO `tasks` (`task_id`, `task_name`, `task_order`)
+VALUES
+	(1,'boards',NULL),
+	(2,'layout',NULL),
+	(3,'animatic',NULL),
+	(4,'lighting',NULL),
+	(6,'animation',NULL);
+
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `ip_address` varbinary(16) NOT NULL,
+  `username` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `password` varchar(80) CHARACTER SET latin1 NOT NULL,
+  `salt` varchar(40) CHARACTER SET latin1 DEFAULT NULL,
+  `email` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `activation_code` varchar(40) CHARACTER SET latin1 DEFAULT NULL,
+  `forgotten_password_code` varchar(40) CHARACTER SET latin1 DEFAULT NULL,
+  `forgotten_password_time` int(11) unsigned DEFAULT NULL,
+  `remember_code` varchar(40) CHARACTER SET latin1 DEFAULT NULL,
+  `created_on` int(11) unsigned NOT NULL,
+  `last_login` int(11) unsigned DEFAULT NULL,
+  `active` tinyint(1) unsigned DEFAULT NULL,
+  `first_name` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
+  `last_name` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
+  `company` varchar(100) CHARACTER SET latin1 DEFAULT NULL,
+  `phone` varchar(20) CHARACTER SET latin1 DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+
+INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `salt`, `email`, `activation_code`, `forgotten_password_code`, `forgotten_password_time`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`)
+VALUES
+	(1,X'7F000001','admin','59beecdf7fc966e2f17fd8f65a4a9aeb09d4a3d4','9462e8eee0','admin@admin.com','',NULL,NULL,NULL,1268889823,1385079851,1,'Admin','istrator','ADMIN','123-123-2123');
+
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table users_groups
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users_groups`;
+
+CREATE TABLE `users_groups` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` mediumint(8) unsigned NOT NULL,
+  `group_id` mediumint(8) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `users_groups` WRITE;
+/*!40000 ALTER TABLE `users_groups` DISABLE KEYS */;
+
+INSERT INTO `users_groups` (`id`, `user_id`, `group_id`)
+VALUES
+	(11,1,1),
+	(12,1,2),
+	(13,1,3),
+	(15,2,2),
+	(19,3,1),
+	(20,3,2),
+	(22,4,1),
+	(23,4,2),
+	(24,2,1);
+
+/*!40000 ALTER TABLE `users_groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ install/index.php
@@ -0,0 +1,121 @@
+<?php
+
+/**
+* Installer v 1.0
+*
+* Installs Attract
+*
+* @author Francesco Siddi
+* @package Attract
+*/
+
+//error_reporting(E_NONE); //Setting this to E_ALL showed that that cause of not redirecting were few blank lines added in some php files.
+
+$db_config_path = '../application/config/database.php';
+
+// Only load the classes in case the user submitted the form
+if($_POST) {
+
+	// Load the classes and create the new objects
+	require_once('includes/core_class.php');
+	require_once('includes/database_class.php');
+
+	$core = new Core();
+	$database = new Database();
+
+
+	// Validate the post data
+	if($core->validate_post($_POST) == true)
+	{
+
+		// First create the database, then create tables, then write config file
+		if($database->create_database($_POST) == false) {
+			$message = $core->show_message('error',"The database could not be created, please verify your settings.");
+		} else if ($database->create_tables($_POST) == false) {
+			$message = $core->show_message('error',"The database tables could not be created, please verify your settings.");
+		} else if ($core->write_databse_config($_POST) == false) {
+			$message = $core->show_message('error',"The database configuration file could not be written, please chmod application/config/database.php file to 777");
+		} else if ($core->write_app_config($_POST) == false) {
+			$message = $core->show_message('error',"The application configuration file could not be written, please chmod application/config/config.php file to 777");
+		}
+
+		// If no errors, redirect to registration page
+		if(!isset($message)) {
+		  $redir = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+      $redir .= "://".$_SERVER['HTTP_HOST'];
+      $redir .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+      $redir = str_replace('install/','',$redir); 
+			header( 'Location: ' . $redir . 'shots' ) ;
+		}
+
+	}
+	else {
+		$message = $core->show_message('error','Not all fields have been filled in correctly. The host, username, password, and database name are required.');
+	}
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+
+		<title>Install | Attract</title>
+	</head>
+	<body>
+	<div class="container">
+		<div class="row">
+			
+	        <div class="col-md-6 col-md-offset-3">
+	        	<div class="page-header">
+					<h1>Attract 2.0 Installation</h1>
+					<p class="lead">Inser the <i>database</i> credentials to install Attract</p>
+				</div>
+	        	
+			    <?php if(is_writable($db_config_path)){?>
+			
+					<?php if(isset($message)) {echo '<p class="error">' . $message . '</p>';}?>
+					  
+					<form class="form-horizontal" role="form" id="install_form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+						<input type="hidden" name="base_url" value="<?php echo $_SERVER['SERVER_NAME']; ?>" />
+						<div class="form-group">
+							<label class="col-sm-4 control-label">Hostname</label>
+							<div class="col-sm-8">
+						      	<input type="text" id="hostname" value="localhost" class="form-control" name="hostname" />
+						    </div>
+						</div>
+						<div class="form-group">
+						   	<label class="col-sm-4 control-label">Username</label>
+						    <div class="col-sm-8">
+						    	<input type="text" id="username" class="form-control" name="username" />
+						    </div>
+						</div>
+						<div class="form-group">
+						    <label for="inputPassword" class="col-sm-4 control-label">Password</label>
+						    <div class="col-sm-8">
+							    <input type="password" id="password" class="form-control" name="password" />
+						    </div>
+						</div>
+						<div class="form-group">
+						    <label class="col-sm-4 control-label">Name</label>
+						    <div class="col-sm-8">
+							    <input type="text" id="database" class="form-control" name="database" />
+						    </div>
+						</div>
+						<div class="form-group">
+						    <div class="col-sm-8 col-sm-offset-4">
+							    <button class="btn btn-primary" type="submit" value="Install" id="submit">Install Attract</button>
+						    </div>
+						</div>
+					</form>
+			
+				  <?php } else { ?>
+			      <p class="error">Please make the application/config/database.php file writable. <strong>Example</strong>:<br /><br /><code>chmod 777 application/config/database.php</code></p>
+				  <?php } ?>
+				        	
+	        </div>
+      	</div>
+	</div>
+	</body>
+</html>
